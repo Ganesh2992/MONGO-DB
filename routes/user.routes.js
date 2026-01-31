39a7767bdf8efa5c4ef59262bb7ad43082c9cc00
Ganesh2992 <ganeshmangalamsinha@gmail.com>
2026-01-31 06:48:29 +0530

    Updated Routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,3 +1,4 @@
+/*
 import express from 'express';
 import {User} from '../models/user.model.js';
 import { randomBytes,createHmac } from 'crypto';
@@ -59,6 +60,81 @@ router.post('/login',async(req,res)=>{
     const token = jwt.sign(payload,process.env.JWT_SECRET);
     return res.json({status: 'success',token});
 });
+export default router;
 
+*/
+
+import express from 'express';
+import {User} from '../models/user.model.js';
+import {randomBytes,createHmac} from 'crypto';
+import jwt from 'jsonwebtoken';
+import { ensureAuthenticated } from '../Middlewares/auth.middleware.js';
+
+
+const router = express.Router();
+
+router.patch('/',ensureAuthenticated,async(req,res)=>{
+    const {name} = req.body;
+    await User.findByIdAndUpdate(req.user._id,{
+      name,
+    });
+
+    return res.json({status: `success`});
+});
+
+
+router.post('/signup',async(req,res)=>{
+    const {name,email,password}  = req.body;
+
+    const existingUser = await User.findOne({
+      email,
+    });
+
+    if(existingUser){
+      return res.status(400).json({error: `This emails ${email} is already exists`});
+    }
+    const salt = randomBytes(256).toString('hex')
+    const hashedPassword= createHmac('sha256',salt)
+    .update(password).digest('hex');
+
+    const user = await User.insertOne({
+      name,
+      email,
+      password:hashedPassword,
+      salt,
+    });
+
+    return res.status(201).json({status: 'success',data:{id: user._id}});
+});
+
+router.post('/login',async(req,res)=>{
+  const {email,password}  =req.body;
+
+  const existingUser = await User.findOne({
+    email,
+    });
+  if (!existingUser){
+    return res.status(400).json({error : `User with email ${email} does not exist `});
+  }
+
+  const salt = existingUser.salt;
+  const hashed = existingUser.password;
+
+  const newHash = createHmac('sha256',salt).update(password).digest('hex');
+
+  if(hashed!==newHash){
+    return res.status(400).json({error: `Invalid Password`});
+  }
+
+  const payload = {
+    name: existingUser.name,
+    _id: existingUser._id,
+    email: existingUser.email,
+  }
+
+  const token = jwt.sign(payload,process.env.JWT_SECRET);
+
+  return res.json({status: 'success',token});
+});
 
 export default router;
